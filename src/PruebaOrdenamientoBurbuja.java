703894c02b431f8d4d1334e3c8f8c2d623255b91
Cristofer Casas <cristofer32513@gmail.com>
2018-11-14 21:06:06 -0600

    Se agrego el metodo ordenamientoBurbuja0. Se agregaron las variables que van a contener los datos para comparar la eficiencia de cada metodo. Se implementaron las variables para comparar la eficiencia en el metodo ordenamientoBurbuja0. se realizaron pruebas con el metodo ordenamientoBurbuja0.

--- src/PruebaOrdenamientoBurbuja.java
+++ src/PruebaOrdenamientoBurbuja.java
@@ -1,16 +1,62 @@
 import java.util.Arrays;
 
 class MetodoDeOrdenamientoBurbuja{
+	int contadorComparaciones;
+	int contadorIntercambios;
+	int contadorRecorridos;
+	long tiempoEjecucion;
 	
 	public void mostrarVector(int[] datos){
 		System.out.println(Arrays.toString(datos));
 	}	
+	
+	public void ordenamientoBurbuja0(int[] datos){
+		contadorComparaciones=0;
+		contadorIntercambios=0;
+		contadorRecorridos=0;
+		tiempoEjecucion=0;
+		long tiempoInicio=0;
+		long tiempoFin=0;
+		
+		int aux=0;
+		tiempoInicio=System.currentTimeMillis();
+		for(int i=0;i<datos.length;i++){
+			for(int j=(i+1);j<datos.length;j++){
+				contadorComparaciones++;
+				if(datos[i]>datos[(j)]){
+					contadorIntercambios++;
+					aux=datos[i];
+					datos[i]=datos[j];
+					datos[j]=aux;
+				}
+			}
+			contadorRecorridos++;
+		}
+		tiempoFin=System.currentTimeMillis();
+		tiempoEjecucion=tiempoFin-tiempoInicio;
+	}
 	
 }
 
 public class PruebaOrdenamientoBurbuja {
 
 	public static void main(String[] args) {
+		int[] edades={34, 25, 12, 87, 9, 10, 34, 37, 24, 2};
 		
+		MetodoDeOrdenamientoBurbuja burbuja=new MetodoDeOrdenamientoBurbuja();
+		
+		System.out.println("=================VECTOR ORIGINAL======================");
+		burbuja.mostrarVector(edades);
+		System.out.println();
+		System.out.println("===============ORDENAMIENTO BURBUJA====================");
+		burbuja.ordenamientoBurbuja0(edades);
+		burbuja.mostrarVector(edades);
+				
+		System.out.println("DATOS DE EFICICENCIA DEL ALGORITMO");
+		System.out.println();
+		System.out.println("    - Cantidad de recorridos realizados: "+burbuja.contadorComparaciones);
+		System.out.println("    - Cantidad de comparaciones realizadas: "+burbuja.contadorComparaciones);
+		System.out.println("    - Cantidad de intercambios realizados: "+burbuja.contadorIntercambios);
+		System.out.println("    - Tiempo total de ejecucion: "+burbuja.tiempoEjecucion);
 	}
 }
