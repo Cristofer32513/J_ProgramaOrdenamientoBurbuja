4aa3d6597690a6e228c916486d64ebad3727c67e
Cristofer Casas <cristofer32513@gmail.com>
2018-11-14 22:17:24 -0600

    Se agrego el metodo mostrarDatosDeEficiencia.

--- src/PruebaOrdenamientoBurbuja.java
+++ src/PruebaOrdenamientoBurbuja.java
@@ -1,25 +1,28 @@
 import java.util.Arrays;
 
 class MetodoDeOrdenamientoBurbuja{
-	int contadorComparaciones;
-	int contadorIntercambios;
-	int contadorRecorridos;
-	long tiempoEjecucion;
 	
 	public void mostrarVector(int[] datos){
 		System.out.println(Arrays.toString(datos));
 	}	
 	
+	public void mostrarDatosDeEficiencia(int contadorComparaciones,
+			int contadorIntercambios, int contadorRecorridos, long tiempoTotal){
+		System.out.println("       DATOS DE EFICIENCIA DEL ALGORITMO");
+		System.out.println();
+		System.out.println("    - Cantidad de recorridos realizados: "+contadorComparaciones);
+		System.out.println("    - Cantidad de comparaciones realizadas: "+contadorComparaciones);
+		System.out.println("    - Cantidad de intercambios realizados: "+contadorIntercambios);
+		System.out.println("    - Tiempo total de ejecucion: "+tiempoTotal);
+		System.out.println("    - Tiempo total de ejecucion: "+(double)tiempoTotal/1000000+" milisegundos");
+	}
+	
 	public void ordenamientoBurbuja0(int[] datos){
-		contadorComparaciones=0;
-		contadorIntercambios=0;
-		contadorRecorridos=0;
-		tiempoEjecucion=0;
-		long tiempoInicio=0;
-		long tiempoFin=0;
+		int contadorComparaciones=0, contadorIntercambios=0, contadorRecorridos=0;
+		long tiempoTotal=0, tiempoInicial=0;
 		
 		int aux=0;
-		tiempoInicio=System.currentTimeMillis();
+		tiempoInicial=System.nanoTime();
 		for(int i=0;i<datos.length;i++){
 			for(int j=(i+1);j<datos.length;j++){
 				contadorComparaciones++;
@@ -32,21 +35,36 @@ class MetodoDeOrdenamientoBurbuja{
 			}
 			contadorRecorridos++;
 		}
-		tiempoFin=System.currentTimeMillis();
-		tiempoEjecucion=tiempoFin-tiempoInicio;
+		tiempoTotal=System.nanoTime()-tiempoInicial;
+		mostrarVector(datos);
+		System.out.println();
+		System.out.println();
+		mostrarDatosDeEficiencia(contadorComparaciones, contadorIntercambios, contadorRecorridos, tiempoTotal);
 	}
 	
 	public void ordenamientoBurbuja1(int[] datos){
+		int contadorComparaciones=0, contadorIntercambios=0, contadorRecorridos=0;
+		long tiempoTotal=0, tiempoInicial=0;
+		
 		int aux=0;
+		tiempoInicial=System.nanoTime();
 		for(int i=2;i<=datos.length;i++){
 			for(int j=0;j<=(datos.length-i);j++){
+				contadorComparaciones++;
 				if(datos[j]>datos[j+1]){
+					contadorIntercambios++;
 					aux=datos[j];
 					datos[j]=datos[j+1];
 					datos[j+1]=aux;
 				}
 			}
+			contadorRecorridos++;
 		}
+		tiempoTotal=System.nanoTime()-tiempoInicial;
+		mostrarVector(datos);
+		System.out.println();
+		System.out.println();
+		mostrarDatosDeEficiencia(contadorComparaciones, contadorIntercambios, contadorRecorridos, tiempoTotal);
 	}
 }
 
@@ -60,21 +78,12 @@ public class PruebaOrdenamientoBurbuja {
 		System.out.println("=================VECTOR ORIGINAL======================");
 		burbuja.mostrarVector(edades);
 		System.out.println();
+		System.out.println();
 		System.out.println("===============ORDENAMIENTO BURBUJA===================");
-		//burbuja.ordenamientoBurbuja0(edades);
-		//burbuja.mostrarVector(edades);
-		System.out.println();
-		System.out.println();
-		
-		/*System.out.println("DATOS DE EFICICENCIA DEL ALGORITMO DE ORDENAMIENTOBURBUJA0");
-		System.out.println();
-		System.out.println("    - Cantidad de recorridos realizados: "+burbuja.contadorComparaciones);
-		System.out.println("    - Cantidad de comparaciones realizadas: "+burbuja.contadorComparaciones);
-		System.out.println("    - Cantidad de intercambios realizados: "+burbuja.contadorIntercambios);
-		System.out.println("    - Tiempo total de ejecucion: "+burbuja.tiempoEjecucion);*/
-		
+		burbuja.ordenamientoBurbuja0(edades);
+			
 		burbuja.ordenamientoBurbuja1(edades);
-		burbuja.mostrarVector(edades);
+				
 		
 		
 		
