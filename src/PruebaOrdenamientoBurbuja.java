6da76d55d015994713977012cdf68b537ee8070b
Cristofer Casas <cristofer32513@gmail.com>
2018-11-14 20:50:12 -0600

    Se agrego el metodo mostrarVector.

--- src/PruebaOrdenamientoBurbuja.java
+++ src/PruebaOrdenamientoBurbuja.java
@@ -1,3 +1,13 @@
+import java.util.Arrays;
+
+class MetodoDeOrdenamientoBurbuja{
+	
+	public void mostrarVector(int[] datos){
+		System.out.println(Arrays.toString(datos));
+	}	
+	
+}
+
 public class PruebaOrdenamientoBurbuja {
 
 	public static void main(String[] args) {
